00cca1064fbeeaccca67833f4bc9c1ed44f706f4
Vincent Chang <vincenttchang90@gmail.com>
2017-09-09 23:02:03 -0400

    first commit

--- designs.js
+++ designs.js
@@ -1,10 +1,31 @@
-// Select color input
-// Select size input
+let color = $('#colorPicker').val();
 
-// When size is submitted by the user, call makeGrid()
+$('#colorPicker').on('change', function() {
+    console.log('we changed the color!');
+    color = $('#colorPicker').val();
+});
 
-function makeGrid() {
-
-// Your code goes here!
-
+//Dynamically creates x by y grid
+function makeGrid(x,y) {
+    $('#pixel_canvas').empty();
+    for(let i=0; i<y; i++){
+        let row = $('<tr>');
+        $('#pixel_canvas').append(row);
+        for(let j=0; j<x; j++){
+            $(row).append('<td class="cell"></td>');
+        }
+    }
 }
+
+//Attaches an event listener to submit button and each cell inside grid
+$('#sizePicker').on('submit', function(e) {
+    e.preventDefault();
+    console.log('calling make Grid');
+    let x = $('#input_width').val();
+    let y = $('#input_height').val();
+    makeGrid(x,y);
+    $('.cell').click(function() {
+        console.log('wtf');
+        $(this).css('background', color);
+    });
+});
